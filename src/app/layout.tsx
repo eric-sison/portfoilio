570884c923ae48c62e6e98e6afae643153279929
eric-sison <ericsison.dev@gmail.com>
2025-08-07 17:01:13 +0800

    made modifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { PropsWithChildren } from "react";
+import { DotBackground } from "@/components/DotBackground";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +21,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <DotBackground>{children}</DotBackground>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/DotBackground.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React, { FunctionComponent, type PropsWithChildren } from "react";
+
+export const DotBackground: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="bg-background relative z-0 flex h-full w-full items-center justify-center">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#262626_1px,transparent_1px)]",
+        )}
+      />
+
+      <div className="bg-background pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="z-10 w-full">{children}</div>
+    </div>
+  );
+};
